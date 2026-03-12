905b69d77ff70c80ab296271908d927667675ac6
Robert McDowell <doodersrage@gmail.com>
2026-03-12 11:20:57 -0400

    styling init

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ export default defineConfig({
     server: {
         host: true, // Listens on all IPs (0.0.0.0)
         port: 4321, // Optional: specify port
-  }
+    },
+  build: { inlineStylesheets: 'never' }
 });
