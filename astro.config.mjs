ef54c4b8e83ff6e9979828f73e43635a90cd084e
Robert McDowell <doodersrage@gmail.com>
2026-03-13 15:50:05 -0400

    first astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        host: true, // Listens on all IPs (0.0.0.0)
-        port: 4321, // Optional: specify port
-    },
+  server: {
+      host: true, // Listens on all IPs (0.0.0.0)
+      port: 4321, // Optional: specify port
+  },
+
   build: { inlineStylesheets: 'never' },
-  site: "https://example.com"
+  site: "https://example.com",
+  integrations: [preact()]
 });
