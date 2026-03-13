540829bb03c943683b21fd514864801deeab31ec
Robert McDowell <doodersrage@gmail.com>
2026-03-13 15:38:02 -0400

    rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ export default defineConfig({
         host: true, // Listens on all IPs (0.0.0.0)
         port: 4321, // Optional: specify port
     },
-  build: { inlineStylesheets: 'never' }
+  build: { inlineStylesheets: 'never' },
+  site: "https://example.com"
 });
